46727205f3b6eb4607d0f6839c7af246209c66ca
SalmanSaeed24a <95068089+SalmanSaeed24a@users.noreply.github.com>
2021-11-29 13:54:20 +0500

    if else

---
+++ else.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b sum;
+char c;
+printf("enter the number:a");
+scanf("%d,&a);
+printf("enter the number:b");
+scanf("%d,&b);
+printf("enter the number:c");
+scanf("%d,&c);
+sum=a+b;
+if(c=='+') 
+printf("sum %d",sum);
+}
